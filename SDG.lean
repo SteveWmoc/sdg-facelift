fffea1c8772b611f88790c3de43b2883bb335a78
Steven Sabean <steves1962wmoc@gmail.com>
2026-08-20 14:51:37 -0500

    feat: import upstream SDG module graph

---
+++ SDG.lean
@@ -0,0 +1,16 @@
+module
+
+public import SDG.Axiom.BigOperators
+public import SDG.Axiom.Instances
+public import SDG.Axiom.UniqueChoice
+public import SDG.Basic.D
+public import SDG.Basic.Defs
+public import SDG.ForMathlib.Fin
+public import SDG.ForMathlib.PiIic
+public import SDG.IsKockLawvere.Taylor
+public import SDG.IsKockLawvere.TaylorMulti
+public import SDG.IsKockLawvere_one.Basic
+public import SDG.IsKockLawvere_one.Deriv
+public import SDG.IsKockLawvere_one.EM
+public import SDG.IsKockLawvere_one.PartialDeriv
+public import SDG.Linters.choice
