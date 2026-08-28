4f384af40d3e97b3aaa01923b0fb232587b05159
Steven Sabean <steves1962wmoc@gmail.com>
2026-08-28 11:16:51 -0500

    chore: remove diagnostic imports

--- SDG.lean
+++ SDG.lean
@@ -1,13 +1,10 @@
 module
 
 public import SDG.Axiom.BigOperators
-public import SDG.Axiom.ChoiceAudit
 public import SDG.Axiom.Instances
 public import SDG.Axiom.UniqueChoice
 public import SDG.Basic.D
 public import SDG.Basic.Defs
-public import SDG.ForMathlib.FieldChoiceFree
-public import SDG.ForMathlib.FieldScalarChoiceFree
 public import SDG.ForMathlib.Fin
 public import SDG.ForMathlib.FinChoiceFree
 public import SDG.ForMathlib.PiIic
